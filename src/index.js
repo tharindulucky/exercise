e9c61088da4272961ac32998111d24f158da7438
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-12-01 02:27:15 +0530

    task 1 completed

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+
+//Task 1
+for(let i = 1; i <= 100; i++){
+    console.log(i+": "+getRandomWordSync());
+}
