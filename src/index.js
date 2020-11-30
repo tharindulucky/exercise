517952211163ac96967bbeb7c11c22a223d9cd47
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-12-01 02:31:19 +0530

    Task 2 completed

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+
+//Task 2
+for(let i = 1; i <= 100; i++){
+
+    let myWord = "";
+
+    if(!(i%3) && !(i%5))
+        myWord = "FizzBuzz";
+    else if(!(i%3))
+        myWord = "Fizz";
+    else if(!(i%5))
+        myWord = "Buzz";
+    else
+        myWord = getRandomWordSync();
+
+    console.log(i+": "+myWord);
+}
