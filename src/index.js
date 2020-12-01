0c4d4d79f0e3f7721446a841af8d65cea4661455
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-12-02 01:06:44 +0530

    wrapped in functions

--- src/index.js
+++ src/index.js
@@ -8,36 +8,47 @@ console.log('It works!');
 //Task 3
 
 /*Task 1 asynchronously */
-const randomWordsArr = [];
+function task1Async() {
+    const randomWordsArr = [];
 
-for(let i = 1; i <= 100; i++){
-    randomWordsArr.push(getRandomWord());
+    for(let i = 1; i <= 100; i++){
+        randomWordsArr.push(getRandomWord());
+    }
+
+    Promise.all(randomWordsArr).then((results) =>{
+        results.map((result, index) => {
+            console.log(index+1+": "+result);
+        });
+    });
 }
 
-Promise.all(randomWordsArr).then((results) =>{
-    results.map((result, index) => {
-        console.log(index+1+": "+result);
-    });
-});
+
 
 
 /*Task 2 asynchronously */
-const fizzBuzzWordsArr = [];
+function task2Async() {
+    const fizzBuzzWordsArr = [];
 
-for(let i = 1; i <= 100; i++){
-    fizzBuzzWordsArr.push(getRandomWord());
+    for(let i = 1; i <= 100; i++){
+        fizzBuzzWordsArr.push(getRandomWord());
+    }
+
+    Promise.all(fizzBuzzWordsArr).then((results) =>{
+        results.map((result, index) => {
+            let newIndex = index+1;
+            if(!(newIndex%3) && !(newIndex%5))
+                console.log(newIndex+": "+"FizzBuzz");
+            else if(!(newIndex%3))
+                console.log(newIndex+": "+"Fizz");
+            else if(!(newIndex%5))
+                console.log(newIndex+": "+"Buzz");
+            else
+                console.log(newIndex+": "+result);
+        });
+    });
 }
 
-Promise.all(fizzBuzzWordsArr).then((results) =>{
-    results.map((result, index) => {
-        let newIndex = index+1;
-        if(!(newIndex%3) && !(newIndex%5))
-            console.log(newIndex+": "+"FizzBuzz");
-        else if(!(newIndex%3))
-            console.log(newIndex+": "+"Fizz");
-        else if(!(newIndex%5))
-            console.log(newIndex+": "+"Buzz");
-        else
-            console.log(newIndex+": "+result);
-    });
-});
+//Calling the functions
+
+task1Async();
+task2Async();
