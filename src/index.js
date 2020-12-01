4913fd28fee8bb055e162e25bbc08c21c63af963
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-12-02 01:50:24 +0530

    added task 4 async methods with error handling to time measurement

--- src/index.js
+++ src/index.js
@@ -55,7 +55,62 @@ function task2Async() {
     });
 }
 
+
+
+/*Task 1 - Asynchronous solutions with Error handling*/
+function task1AsyncWithErrorHandling() {
+
+    console.time("task1AsyncWithErrorHandling");
+
+    const randomWordsArr = [];
+
+    for(let i = 1; i <= 100; i++){
+        randomWordsArr.push(getRandomWord({ withErrors: true, slow: true }).catch(e => "It shouldn't break anything!"));
+    }
+
+    Promise.all(randomWordsArr).then((results) =>{
+        results.map((result, index) => {
+            console.log(index+1+": "+result);
+        });
+        console.timeEnd("task1AsyncWithErrorHandling");
+    });
+}
+
+
+
+/*Task 2 - Asynchronous solutions with Error handling*/
+function task2AsyncWithErrorHandling() {
+
+    console.time("task2AsyncWithErrorHandling");
+
+    const fizzBuzzWordsArr = [];
+
+    for(let i = 1; i <= 100; i++){
+        fizzBuzzWordsArr.push(getRandomWord({ withErrors: true, slow: true }).catch(e => "It shouldn't break anything!"));
+    }
+
+    Promise.all(fizzBuzzWordsArr).then((results) =>{
+        results.map((result, index) => {
+            let newIndex = index+1;
+            if(!(newIndex%3) && !(newIndex%5))
+                console.log(newIndex+": "+"FizzBuzz");
+            else if(!(newIndex%3))
+                console.log(newIndex+": "+"Fizz");
+            else if(!(newIndex%5))
+                console.log(newIndex+": "+"Buzz");
+            else
+                console.log(newIndex+": "+result);
+        });
+        console.timeEnd("task2AsyncWithErrorHandling");
+    });
+}
+
+
+
+
 //Calling the functions
 
 task1Async();
 task2Async();
+task1AsyncWithErrorHandling();
+task2AsyncWithErrorHandling();
