f5d2291c82cbdbb8b00a251f25c5c89e6eb64cf7
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-12-02 20:27:06 +0530

    added instructions comments on index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+
+/*Please switch to branches for each task.
+*
+* Branches:
+*
+* task_1
+* task_2
+* task_3
+* task_4
+* task_5
+* measure_time
+*
+*
+* I have updated the README file for special instructions.
+*
+*  */
